93013a4c0e9038ffd71c1d77e818107bd430c56e
BluRay Fox <266079442+BluRayFox@users.noreply.github.com>
2026-08-22 18:09:54 +0300

    Engine update pull

--- Game/main.lua
+++ Game/main.lua
@@ -3,3 +3,7 @@ local Renderer = Foxit:GetRenderer()
 local Play = Object2D.new()
 Play:LoadImage('Game/Assets/Play.png')
 Play.Scale = Vector2.new(0.4, 0.3)
+
+Play.OnMouseClicked:Connect(function()
+    print('lol')
+end)
